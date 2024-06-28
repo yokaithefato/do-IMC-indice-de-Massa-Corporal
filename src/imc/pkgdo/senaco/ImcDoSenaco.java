e2631e657612871887ef331717cf7995d2df43a9
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-06-28 16:02:40 -0300

    add code

---
+++ src/imc/pkgdo/senaco/ImcDoSenaco.java
@@ -0,0 +1,29 @@
+
+package imc.pkgdo.senaco;
+
+ import java.util.Scanner;
+
+public class ImcDoSenaco {
+
+    
+    public static void main(String[] args) {
+        
+          Scanner sc = new Scanner (System.in);
+     
+          System.out.println("informe a altura do cidadao colega");   
+  double a = sc.nextDouble();
+  
+  double A = a*a;
+  
+  System.out.println("informe o peso desse cabaco");
+  double P = sc.nextDouble();
+  
+  double IMC = P/A;
+  
+  System.out.println("o peso do cidadao e " + IMC);
+  
+
+          
+    }
+    
+}
